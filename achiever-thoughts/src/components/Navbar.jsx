7f9249f7da00c795409b672dfae1ba328ee514c4
achieverthoughts <achieverthoughts@outlook.com>
2026-02-01 06:56:31 +0530

    Nav bar implemented as different component

---
+++ achiever-thoughts/src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+﻿import { useState } from "react";
+import "../styles/navbar.css";
+import logo from "../assets/ATLogo-Icon.png"; 
+
+export default function Navbar() {
+    const [open, setOpen] = useState(false);
+
+    return (
+        <header className="navbar">
+            <div className="navbar-inner container">
+                {/*<a href="/#/" className="brand">*/}
+                {/*    <img*/}
+                {/*        src={logo}*/}
+                {/*        alt="Achiever Thoughts logo"*/}
+                {/*        className="brand-logo"*/}
+                {/*    />*/}
+                    
+                {/*</a>*/}
+
+                <button
+                    className="nav-toggle"
+                    aria-label="Toggle navigation"
+                    aria-expanded={open}
+                    onClick={() => setOpen(!open)}
+                >
+                    Menu
+                </button>
+
+                <span className="brand-text">
+                    Achiever Thoughts
+                </span>
+
+                <nav
+                    className={`nav ${open ? "open" : ""}`}
+                    aria-label="Main navigation"
+                >
+                    <a href="/#/">Home</a>
+                    <a href="/#/products">Products</a>
+                    <a href="/#/demos">Demos</a>
+                    <a href="/#/investors">Investors</a>
+                </nav>
+            </div>
+        </header>
+    );
+}
